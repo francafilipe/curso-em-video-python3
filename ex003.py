e27994c345db0b13a26c04386dd2ff9a10b74ce9
Filipe França <filipeffranc@gmail.com>
2018-08-06 20:06:26 -0300

    Create exercise 3

---
+++ ex003.py
@@ -0,0 +1,8 @@
+# Curso em Vídeo Python 3 - Exercicio 3
+# Programa que recebe dois numeros e mostra o resultado da soma
+
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+soma = num1 + num2
+
+print('A soma entre {} e {} é igual a {}!'.format(num1, num2, soma))
