c3f774106ca3f66263287840208b4d3ca4d1bd1d
Filipe França <filipeffranc@gmail.com>
2018-08-06 19:16:17 -0300

    Create exercise 2

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Curso em Vídeo Python 3 - Exercício 2
+# Programa que recebe o nome do usuário e mostra uma mensagem
+
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer, ' + nome + '!')
