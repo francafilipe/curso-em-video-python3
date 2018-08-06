937a89bb8f91737c05062ff3de40881334a444ab
Filipe França <filipeffranc@gmail.com>
2018-08-06 20:57:58 -0300

    Create exercise 8

---
+++ ex008.py
@@ -0,0 +1,8 @@
+# Curso em Vídeo Python 3 - Exercicio 8
+# Converção de metros para centímetros e milimetros
+
+valor = float(input('Digite um valor em metros: '))
+cent = valor*100
+mil = valor*1000
+
+print('O valor em centimetros é {}cm e em milimetros é {}mm.'.format(cent, mil))
