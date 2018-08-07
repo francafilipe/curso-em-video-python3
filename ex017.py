5493c80d9580c97b7d50c749cb105f04251e027e
Filipe França <filipeffranc@gmail.com>
2018-08-07 15:36:17 -0300

    Change solution in exercise 17

--- ex017.py
+++ ex017.py
@@ -1,9 +1,9 @@
 # Curso em Vídeo Python 3 - Exercicio 17
 # Calculo da hipotenusa de um triângulo retangulo
 
-from math import sqrt, pow
+from math import hypot
 
-cat_oposto = float(input('Valor do cateto oposto: '))  # Recebe o valor do cateto oposto do triangulo
+cat_op = float(input('Valor do cateto oposto: '))  # Recebe o valor do cateto oposto do triangulo
 cat_adj = float(input('Valor do cateto adjacente: '))  # Recebe o valor do cateto adjacente do triangulo
-hip = sqrt(pow(cat_adj, 2) + pow(cat_oposto, 2))  # Calcula o valor da hipotenusa pelo teorema de Pitagoras
+hip = hypot(cat_op, cat_adj)  # Calcula o valor da hipotenusa pelo teorema de Pitagoras
 print('O valor da hipotenusa do triangulo é {:.2}'.format(hip))
