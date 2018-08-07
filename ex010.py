b4639efe8f45a686dc6bf6425029b94daf31caa0
Filipe França <filipeffranc@gmail.com>
2018-08-06 21:12:56 -0300

    Create exercise 10

---
+++ ex010.py
@@ -0,0 +1,7 @@
+# Curso em Vídeo Python 3 - Exercicio 10
+# Conversão de real para dólar
+
+cot = 3.27
+valor = float(input('Digite o valor em real: '))
+dol = valor/cot
+print('O valor em dólar é igual a {:.2f}.'.format(dol))
