5aa74f8ded177b9a9db556d6b97c7d157b2d9e21
Filipe França <filipeffranc@gmail.com>
2018-08-08 17:28:29 -0300

    Add another solution to ex 32

--- ex032.py
+++ ex032.py
@@ -9,3 +9,14 @@ else:
         print('Ano bissexto.')
     else:
         print('Ano não bissexto.')
+
+"""
+Pode ser realizado em somente um comando if
+
+
+ano = int(input('Insira o ano: '))
+if ano % 400 == 0 or (ano % 4 == 0 and ano % 100 != 0):
+    print('Ano bissexto.')
+else:
+    print('Ano não bissexto.')
+"""
