64b13e4375ad153d07b00db3f7cf1204d77620c7
Filipe França <filipeffranc@gmail.com>
2018-08-08 16:44:59 -0300

    Solve exercise 32

---
+++ ex032.py
@@ -0,0 +1,11 @@
+# Curso em Vídeo Python 3 - Exercicio 32
+# Verificação se um ano é bissexto ou não
+
+ano = int(input('Insira o ano: '))
+if ano % 400 == 0:
+    print('Ano bissexto.')
+else:
+    if (ano % 4 == 0 and ano % 100 != 0):
+        print('Ano bissexto.')
+    else:
+        print('Ano não bissexto.')
