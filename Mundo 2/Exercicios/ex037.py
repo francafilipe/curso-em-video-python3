d83761aa6d95dadad3b7a81a15823026d970909e
Filipe França <filipeffranc@gmail.com>
2018-08-09 12:23:27 -0300

    Formatar resolução exericio 37

--- Mundo 2/Exercicios/ex037.py
+++ Mundo 2/Exercicios/ex037.py
@@ -2,11 +2,14 @@
 # Conversão de um número inteiro para binário, hexadecimal ou octal
 
 num = int(input('Digite o número: '))
-base = int(input('Insira (1)binário, (2)octal e (3)hexadecimal: '))
+base = int(input('''Insira a base desejada para conversão:
+( 1 ) Binário
+( 2 ) Octal
+( 3 ) Hexadecimal\n'''))
 
 if base == 1:
-    print('O número {} em binário é: {}'.format(num, bin(num)))
+    print('O número {} em binário é: {}'.format(num, bin(num)[2:]))
 elif base == 2:
-    print('O número {} na base octal é: {}'.format(num, oct(num)))
+    print('O número {} na base octal é: {}'.format(num, oct(num)[2:]))
 elif base == 3:
-    print('O número {} na base hexadecimal é: {}'.format(num, hex(num)))
+    print('O número {} na base hexadecimal é: {}'.format(num, hex(num)[2:]))
