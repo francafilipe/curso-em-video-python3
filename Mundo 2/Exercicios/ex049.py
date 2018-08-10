d16fab3c7dfc9ec838b09e1eab7ea2d3e8d07482
Filipe França <filipeffranc@gmail.com>
2018-08-10 11:06:32 -0300

    Resolver exericio 49

---
+++ Mundo 2/Exercicios/ex049.py
@@ -0,0 +1,11 @@
+# Curso em Vídeo Python 3 - Exercicio 49
+# Tabuada
+
+num = int(input('Digite um número: '))
+
+print('-='*8)
+print('TABUADA DO {}'.format(num))
+for i in range(0, 11):
+    res = num*i
+    print('{} x {} = {}'.format(num, i, res))
+print('-='*8)
