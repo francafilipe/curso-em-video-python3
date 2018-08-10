f126b2c73b1e6ecd0fe282f12056f600c9193613
Filipe França <filipeffranc@gmail.com>
2018-08-10 12:27:01 -0300

    Alterar resolução exercicio 55

--- Mundo 2/Exercicios/ex055.py
+++ Mundo 2/Exercicios/ex055.py
@@ -2,13 +2,15 @@
 # Maior e menor peso
 
 maior = 0
-menor = 500
+menor = 0
 
 for i in range(0, 5):
     peso = float(input('Digite o peso da {}° pessoa: '.format(i+1)))
     if peso > maior:
         maior = peso
-    if peso < menor:
+    if i == 0:
+        menor = peso
+    elif peso < menor:
         menor = peso
 
 print('O maior peso é: {}'.format(maior))
