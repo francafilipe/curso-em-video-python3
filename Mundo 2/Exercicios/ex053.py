faedaa07a16dc3498e189c315946215b358af828
Filipe França <filipeffranc@gmail.com>
2018-08-10 11:55:11 -0300

    Resolver exercicio 53

---
+++ Mundo 2/Exercicios/ex053.py
@@ -0,0 +1,12 @@
+# Curso em Vídeo Python 3 - Exercicio 53
+# Verificador de Palíndromo
+
+frase = input('Insira a frase: ').lower().replace(' ', '')
+invertida = ''
+
+for i in range(len(frase) - 1, -1, -1):
+    invertida += frase[i]
+if frase == invertida:
+    print('A frase é um palíndromo.')
+else:
+    print('A frase não é um palíndromo.')
