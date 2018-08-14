437d9e0a7f4d23cd5dea705a23c4f67acdc4fcce
Filipe França <filipeffranc@gmail.com>
2018-08-14 08:08:25 -0300

    Adicionar solução alternativa Exercicio 62

--- Mundo 2/Exercicios/ex062.py
+++ Mundo 2/Exercicios/ex062.py
@@ -1,6 +1,21 @@
 # Curso em Vídeo Python 3 - Exercicio 62
 # Termos de uma PA
 
+prim_termo = termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+mais = 10
+cont = total_termos = 0
+
+while mais != 0:
+    total_termos += mais
+    while cont < total_termos:
+        print('{}° termo: {}'.format(cont + 1, termo))
+        termo += razao
+        cont += 1
+    mais = int(input('Deseja mais quantos termos: '))
+
+
+"""
 prim_termo = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 cont = tot_termos = 0
@@ -21,3 +36,4 @@ while des != 0:
         tot_termos += 1
     des = int(input('Deseja mostrar mais quantos termos: '))
     tot = tot_termos + des
+"""
