078d1d35a40cf684bdb8e330799ad910a1ab788b
Filipe França <filipeffranc@gmail.com>
2018-09-01 16:44:33 -0300

    Alterar solução exercicio 75

--- Mundo 3/Exercicios/ex075.py
+++ Mundo 3/Exercicios/ex075.py
@@ -6,17 +6,15 @@ num2 = int(input('Digite o segundo numero: '))
 num3 = int(input('Digite o terceiro numero: '))
 num4 = int(input('Digite o quarto numero: '))
 numeros = (num1, num2, num3, num4)
-print('Foi digitados os valores: {}'.format(numeros))
+print('Foram digitados os valores: {}'.format(numeros))
 
-cont9 = 0
 print('Os numeros pares digitados foram: ', end='')
 for pos, num in enumerate(numeros):
     if num % 2 == 0:
         print(num, end=' ')
-    if num == 9:
-        cont9 += 1
-    if num == 3:
-        posicao = pos
 
-print('\nO valor 9 apareceu {} vezes.'. format(cont9))
-print('O valor 3 apareceu na {}° posição'.format(posicao + 1))
+print('\nO valor 9 apareceu {} vezes.'. format(numeros.count(9)))
+if 3 in numeros:
+    print('O valor 3 apareceu na {}° posição'.format(numeros.index(3)))
+else:
+    print('O valor 3 não foi digitado.')
