5e8863d1f3053b5410469f9adf76317f4c04383b
Filipe França <filipeffranc@gmail.com>
2018-09-01 16:32:06 -0300

    Alterar resolução exercicio 72

--- Mundo 3/Exercicios/ex072.py
+++ Mundo 3/Exercicios/ex072.py
@@ -6,9 +6,10 @@ numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
            'doze', 'treze', 'quatorze', 'quinze', 'dezesseis',
            'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-numero = int(input('Digite um número (entre 0 e 20): '))
-
-while 20 < numero < 0:
-    numero = int(input('Entrada inválida. Digite um número (entre 0 e 20): '))
+while True:
+    numero = int(input('Digite um número (entre 0 e 20): '))
+    if 0 <= numero <= 20:
+        break
+    print('Entrada inválida!')
 
 print('Você digitou o número {}'.format(numeros[numero]))
