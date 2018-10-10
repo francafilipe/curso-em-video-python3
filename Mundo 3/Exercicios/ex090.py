daaf34154a6dde6a6bdb361eb6478d1a79c0c424
Filipe França <filipeffranc@gmail.com>
2018-10-10 15:12:04 -0300

    Adicionar solução alternativa ex. 90

--- Mundo 3/Exercicios/ex090.py
+++ Mundo 3/Exercicios/ex090.py
@@ -1,6 +1,7 @@
 # Curso em Vìdeo Python 3 - Exercicio 90
 # Dicionario em Python
 
+"""
 nome = str(input('Nome: '))
 media = float(input('Media: '))
 
@@ -10,6 +11,19 @@ else:
     sit = 'Reprovado'
 
 aluno = {'Nome': nome, 'Média': media, 'Situação': sit}
+
+for k, v in aluno.items():
+    print('{} é {}.'.format(k, v))
+"""
+
+aluno = dict()
+aluno['Nome'] = str(input('Nome: '))
+aluno['Media'] = float(input('Média: '))
+
+if aluno['Media'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+else:
+    aluno['Situação'] = 'Reprovado'
 
 for k, v in aluno.items():
     print('{} é {}.'.format(k, v))
