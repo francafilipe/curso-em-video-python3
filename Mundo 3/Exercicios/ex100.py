9e81b1c4de52227df65915808528c7ca3d85c91a
Filipe França <filipeffranc@gmail.com>
2018-11-21 12:47:19 -0200

    Resolver exercicio 100

---
+++ Mundo 3/Exercicios/ex100.py
@@ -0,0 +1,27 @@
+# Curso em Vídeo Python 3 - Exericio 100
+# Funções para sortear e somar
+
+from random import randint
+
+
+def sortear(n):
+    numeros = list()
+    print('Sorteando valores...')
+    for i in range(0, n):
+        num = randint(0, 10)
+        numeros.append(num)
+    return numeros
+
+
+def soma(numeros):
+    soma = 0
+    for i in numeros:
+        if i % 2 == 0:
+            soma += i
+    return soma
+
+
+n = int(input('Quantos numeros a serem sorteados? '))
+nums = sortear(n)
+print(nums)
+print('A soma dos números pares sorteados é: {}'.format(soma(nums)))
